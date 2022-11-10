758bfea35e25bc2958d3bfdcb40f438002d577bf
abraxas678 <abraxas678@gmail.com>
2022-11-10 15:19:58 +0100

    Update execute.sh

--- execute.sh
+++ execute.sh
@@ -6,6 +6,7 @@ mygrace=3
 path="/usr/bin"
 name=task
 MY_URL=$(/home/abraxas/myq/Auto-provisioning_HealthCheckVAR.io.sh "$(hostname)" "$path" "$name" "$mytime" "$mygrace")
-
-# /home/abraxas/bin/runitor -ping-key="$HC_PING_KEY" -uuid="b79c45fe-8677-4e5f-8c56-30565a2414bb" -api-url="https://hc.dmw.zone/ping" 
-#  /home/abraxas/bin/runitor -api-url="$HC_ROOT_URL/ping" -uuid="$MY_UUID" -ping-key="kDHCdjmjDpr72AOHTdMKBw" -quiet="false" -silent="false" -no-start-ping="false" -no-output-in-ping="false"
+echo "URL RECEIVED: $MY_URL"
+MY_UUID=$(echo $MY_URL | sed 's/.*ping\///' | sed 's/\"//g')
+echo MYUUID $MY_UUID
+/home/abraxas/bin/runitor -api-url="$HC_ROOT_URL/ping" -uuid="$MY_UUID" -ping-key="kDHCdjmjDpr72AOHTdMKBw" -quiet="false" -silent="false" -no-start-ping="false" -no-output-in-ping="false" /usr/bin/task sync
