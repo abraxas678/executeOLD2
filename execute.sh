b6c81e97ca56f5f055ba7926ffb7455776678dce
abraxas678 <abraxas678@gmail.com>
2022-11-10 13:15:54 +0100

    Update execute.sh

--- execute.sh
+++ execute.sh
@@ -5,6 +5,7 @@ mytime=1
 mygrace=3
 path="/usr/bin"
 name=task
-# /home/abraxas/myq/Auto-provisioning_HealthCheckVAR.io.sh "$(hostname)" "$path" "$name" "$mytime" "$mygrace")
+MY_URL=$(/home/abraxas/myq/Auto-provisioning_HealthCheckVAR.io.sh "$(hostname)" "$path" "$name" "$mytime" "$mygrace")
+
 # /home/abraxas/bin/runitor -ping-key="$HC_PING_KEY" -uuid="b79c45fe-8677-4e5f-8c56-30565a2414bb" -api-url="https://hc.dmw.zone/ping" 
 #  /home/abraxas/bin/runitor -api-url="$HC_ROOT_URL/ping" -uuid="$MY_UUID" -ping-key="kDHCdjmjDpr72AOHTdMKBw" -quiet="false" -silent="false" -no-start-ping="false" -no-output-in-ping="false"
