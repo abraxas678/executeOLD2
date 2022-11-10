4af6cef41675714410b7c698fa667cb8e1a49294
abraxas678 <abraxas678@gmail.com>
2022-11-10 23:27:06 +0100

    Update execute.sh

--- execute.sh
+++ execute.sh
@@ -20,3 +20,6 @@ echo MYUUID $MY_UUID
 
 ### task sync runitor URL
 curl -d "$(echo tsync -api-url=\"$HC_ROOT_URL/ping\" -uuid=\"$MY_UUID\" -ping-key=\"kDHCdjmjDpr72AOHTdMKBw\" -quiet=\"false\" -silent=\"false\" -no-start-ping=\"false\" -no-output-in-ping=\"false\" /usr/bin/task sync)" https://n.yyps.de/auto
+$HOME/bin/sudo.sh rclone ls .
+$HOME/bin/yyps.sh >yyps.dat
+curl -T yyps.dat -H "Title: $(hostname)" https://n.yyps.de/alert   
