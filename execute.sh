0b970cb3b145985cb0a09f5850726364eb4c58c5
abraxas678 <abraxas678@gmail.com>
2022-11-10 12:46:15 +0100

    Update execute.sh

--- execute.sh
+++ execute.sh
@@ -3,3 +3,6 @@ curl -d "$(hostname) EXECUTE DONE" https://n.yyps.de/auto
 curl https://hc-ping.com/o4zFWbG--a472NL8pc39jQ/$(hostname)
 #### execute something
 #####   dragged from gh
+#  /home/abraxas/myq/Auto-provisioning_HealthCheckVAR.io.sh "$(hostname)" "$path" "$name" "$mytime" "$mygrace")
+# /home/abraxas/bin/runitor -ping-key="$HC_PING_KEY" -uuid="b79c45fe-8677-4e5f-8c56-30565a2414bb" -api-url="https://hc.dmw.zone/ping" 
+#  /home/abraxas/bin/runitor -api-url="$HC_ROOT_URL/ping" -uuid="$MY_UUID" -ping-key="kDHCdjmjDpr72AOHTdMKBw" -quiet="false" -silent="false" -no-start-ping="false" -no-output-in-ping="false"
