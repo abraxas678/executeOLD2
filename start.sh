6b90f06b1d040b47df586e967ced541f6edf0def
abraxas678 <abraxas678@gmail.com>
2022-11-11 02:08:35 +0100

    1

--- start.sh
+++ start.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
+
+### start durch curl -sL http://execute-setup.yyps.de | bash
+
+ts=$(date +"%s")
+mkdir $HOME/tmp >/dev/null 2>/dev/null
+cd $HOME/tmp
+mkdir $HOME/tmp/$ts
+cd $HOME/tmp/$ts
+git clone https://github.com/abraxas678/execute.git
+cd $HOME/tmp/$ts/execute
+
+wget https://github.com/abraxas678/execute/raw/master/1m-execute-setup.tar
 rm 1m-execute-setup.tar -f
 mkdir /home/abraxas/myq >/dev/null 2>/dev/null
 mkdir /home/abraxas/myq/service >/dev/null 2>/dev/null
