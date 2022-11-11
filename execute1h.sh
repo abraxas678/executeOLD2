161e959133798aac02d1bfef5f10cac349e39cd6
abraxas678 <abraxas678@gmail.com>
2022-11-11 21:54:05 +0100

    1

---
+++ execute1h.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+$HOME/bin/sudo.sh rclone lsd ~/tmp
+/usr/bin/task sync
+
+### am here NTFY auto
+curl -d "$(hostname) $(date) EXECUTE >>>1h<<<" https://n.yyps.de/auto
+
+### am here HC.IO
+curl -d ">>>1h<<<" https://hc-ping.com/o4zFWbG--a472NL8pc39jQ/$(hostname)/log
+
+if [[ $(hostname) = *"aws"* ]]; then
+  echo " "
+fi
