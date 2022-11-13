a249a55c5597985e2872785f44146271a0b9a3c3
abraxas678 <abraxas678@gmail.com>
2022-11-13 19:31:11 +0100

    Update execute1h.sh

--- 1h/execute1h.sh
+++ 1h/execute1h.sh
@@ -9,6 +9,10 @@ curl -d "$(hostname) $(date) EXECUTE >>>1h<<<" https://n.yyps.de/auto
 ### am here HC.IO
 curl -d ">>>1h<<<" https://hc-ping.com/o4zFWbG--a472NL8pc39jQ/$(hostname)/log
 
+if [[ $(hostname) = *"ionos" ]]; then
+  $HOME/bin/bookmarks.sh
+fi
+
 if [[ $(hostname) = *"aws"* ]]; then
   echo " "
 fi
