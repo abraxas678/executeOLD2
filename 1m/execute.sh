51afc2b97d87a5c9e8efe6c2a540586487c14155
abraxas678 <abraxas678@gmail.com>
2022-11-21 09:14:46 +0100

    Update execute.sh

--- 1m/execute.sh
+++ 1m/execute.sh
@@ -7,7 +7,7 @@ ts=$(date +"%s")
 #wget https://raw.githubusercontent.com/abraxas678/myq/master/ONCE/ALL/onceALL.sh
 #chmod +x onceALL.sh
 #/bin/bash onceALL.sh
-/bin/bash $HOME/bin/check-my-tasks.sh
+/home/abraxas/bin/runitor -slug $(hostname)flex check-my-tasks.sh
 
 #cd /home/abraxas/myq/ONCE/$(hostname)
 #wget https://raw.githubusercontent.com/abraxas678/myq/master/ONCE/$(hostname)/once.sh
