15d1604308c183ff1062d1da521df0bf44d59c60
abraxas678 <abraxas678@gmail.com>
2022-11-21 07:23:38 +0100

    1

--- 1m/start.sh
+++ 1m/start.sh
@@ -11,11 +11,11 @@ git clone https://github.com/abraxas678/execute.git
 cd $HOME/tmp/$ts/execute
 
 wget https://github.com/abraxas678/execute/raw/master/1m/1m-execute-setup.tar
-tar -xf 1m-execute-setup.tar
-rm 1m-execute-setup.tar -f
+tar -xf cd $HOME/tmp/$ts/execute/1m-execute-setup.tar
+rm cd $HOME/tmp/$ts/execute/1m-execute-setup.tar -f
 mkdir /home/abraxas/myq >/dev/null 2>/dev/null
 mkdir /home/abraxas/myq/service >/dev/null 2>/dev/null
-mv 1m.sh /home/abraxas/myq/service
-sudo mv ./1m* /etc/systemd/system
+mv $HOME/tmp/$ts/execute/1m.sh /home/abraxas/myq/service
+sudo mv $HOME/tmp/$ts/execute/1m* /etc/systemd/system
 $HOME/bin/sudo.sh systemctl start 1m.timer
 $HOME/bin/sudo.sh systemctl enable 1m.timer
