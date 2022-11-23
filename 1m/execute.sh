46c5691f17d3ea05594484d21906beee16e50125
abraxas678 <abraxas678@gmail.com>
2022-11-23 05:13:43 +0100

    1

--- 1m/execute.sh
+++ 1m/execute.sh
@@ -4,20 +4,9 @@ source /home/abraxas/bin/path.dat
 source /home/abraxas/.zsh.env
 
 ts=$(date +"%s")
-
-#$HOME/bin/sudo.sh rclone lsd ~/tmp
-#cd /home/abraxas/myq/ONCE/ALL
-#wget https://raw.githubusercontent.com/abraxas678/myq/master/ONCE/ALL/onceALL.sh
-#chmod +x onceALL.sh
-#/bin/bash onceALL.sh
+echo $ts
 rm -f /home/abraxas/test.dat
 /home/abraxas/bin/runitor -slug $(hostname)flex /home/abraxas/bin/check-my-tasks.sh >>/home/abraxas/test.dat 2>>/home/abraxas/test.dat
-
-#cd /home/abraxas/myq/ONCE/$(hostname)
-#wget https://raw.githubusercontent.com/abraxas678/myq/master/ONCE/$(hostname)/once.sh
-#chmod +x once.sh
-
-#/bin/bash/ $HOME/bin/processq-1m.sh
 
 ### am here NTFY auto
 curl -d "$(hostname) $(date) EXECUTE $(cat ~/.sync_check//$(ls -la  ~/.sync_check | tail -n 1  | awk '{ print $9 }'))" https://n.yyps.de/auto
@@ -29,3 +18,17 @@ curl https://hc-ping.com/o4zFWbG--a472NL8pc39jQ/$(hostname)
 
 echo $ts >/home/abraxas/myq/last-1m-$(hostname).dat   
 exit
+
+
+#$HOME/bin/sudo.sh rclone lsd ~/tmp
+#cd /home/abraxas/myq/ONCE/ALL
+#wget https://raw.githubusercontent.com/abraxas678/myq/master/ONCE/ALL/onceALL.sh
+#chmod +x onceALL.sh
+#/bin/bash onceALL.sh
+
+#cd /home/abraxas/myq/ONCE/$(hostname)
+#wget https://raw.githubusercontent.com/abraxas678/myq/master/ONCE/$(hostname)/once.sh
+#chmod +x once.sh
+
+#/bin/bash/ $HOME/bin/processq-1m.sh
+
