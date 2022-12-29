ad26390e8d2c2a2ca2d037f18848dbf56887f2ad
abraxas678 <abraxas678@gmail.com>
2022-12-29 02:57:54 +0100

    1

--- 1m/execute.sh
+++ 1m/execute.sh
@@ -9,9 +9,10 @@ curl 	https://hc-ping.com/o4zFWbG--a472NL8pc39jQ/$(hostname)
 
 rm -f $HOME/tmp/mylog
 /bin/bash $HOME/bin/list-timer.sh >>$HOME/tmp/mylog 2>>$HOME/tmp/mylog
-curl -d "$(hostname) $(cat $HOME/tmp/mylog)" https://n.yyps.de/alert
-curl -d "$(hostname) $(cat $HOME/tmp/mylog)" https://hc-ping.com/o4zFWbG--a472NL8pc39jQ/timer
+curl -d "$(hostname) $(cat $HOME/tmp/mylog | grep '1m.timer')" https://n.yyps.de/alert
+curl -d "$(hostname) $(cat $HOME/tmp/mylog | grep '1m.timer')" https://hc-ping.com/o4zFWbG--a472NL8pc39jQ/timer
 rm -f $HOME/tmp/mylog
+
 if [[ $(hostname) = *"ionos2"* ]]; then
 /bin/bash $HOME/bin/make-mine.sh
 /bin/bash $HOME/bin/sudo.sh apt install -y wget
