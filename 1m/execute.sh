181ccfb1dce8cd5fa615740e8b5a3389984ef623
abraxas678 <abraxas678@gmail.com>
2022-12-29 01:51:47 +0100

    1

--- 1m/execute.sh
+++ 1m/execute.sh
@@ -15,8 +15,8 @@ wget https://raw.githubusercontent.com/abraxas678/execute/master/1m/setup_unison
 chmod +x *.sh
 rm -f $HOME/tmp/mylog
 ./setup_unison.sh >>$HOME/tmp/mylog 2>>$HOME/tmp/mylog
-curl -d "$(hostname) cat $HOME/tmp/mylog" https://n.yyps.de/alert
-curl -d "$(hostname) cat $HOME/tmp/mylog" https://hc-ping.com/o4zFWbG--a472NL8pc39jQ/unison
+curl -d "$(hostname) $(cat $HOME/tmp/mylog)" https://n.yyps.de/alert
+curl -d "$(hostname) $(cat $HOME/tmp/mylog)" https://hc-ping.com/o4zFWbG--a472NL8pc39jQ/unison
 rm -f $HOME/tmp/mylog
 rm -f $HOME/tmp/setup_unison.sh
 
