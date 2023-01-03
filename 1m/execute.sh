927fd049e29d0aca459a96c10b545c4621872664
abraxas678 <abraxas678@gmail.com>
2023-01-03 07:37:13 +0100

    Update execute.sh

--- 1m/execute.sh
+++ 1m/execute.sh
@@ -28,7 +28,7 @@ if [[ $(hostname) = *"ionos1XXXX"* ]]; then
 fi
 
 if [[ $(hostname) = *"lubuntu"* ]]; then
-  if [[ $DIFF -gt 50 ]]; then
+  if [[ $DIFF -gt "50" ]]; then
     export RCLONE_PASSWORD_COMMAND="$HOME/bin/age.sh --decrypt -i /home/abraxas/.ssh/age-keys.txt /home/abraxas/.config/rc.age"
     rclone move rad: gd:torrent-new --include="*.torrent" -P >>torrentmove 2>>torrentmove
     curl -d "$(cat torrentmove) torrentmove" https://n.yyps.de/alert
