c7ce9c80c100d05242353910275f828f95437d94
abraxas678 <abraxas678@gmail.com>
2023-01-07 06:45:52 +0000

    1

--- 1m/execute.sh
+++ 1m/execute.sh
@@ -11,7 +11,7 @@ curl -d "execute.sh" https://hc-ping.com/o4zFWbG--a472NL8pc39jQ/$(hostname)
 #curl -d "111111111111" https://n.yyps.de/alert
 
 sudo apt-get install dnsutils  -y
-/home/abraxas/bin/runitor -every=0 -api-url=https://hc-ping.com -slug=IP -ping-key=o4zFWbG--a472NL8pc39jQ -- dig +short myip.opendns.com @resolver1.opendns.com >myip
+/home/abraxas/bin/runitor -every=0 -api-url=https://hc-ping.com -slug=ip -ping-key=o4zFWbG--a472NL8pc39jQ -- dig +short myip.opendns.com @resolver1.opendns.com >myip
 #curl -d "$(hostname)  $(cat myip)" https://n.yyps.de/alert
 rm -f myip
 
@@ -84,31 +84,3 @@ source /home/abraxas/.zsh.env
 
 ts=$(date +"%s")
 echo $ts
-rm -f /home/abraxas/test.dat
-/home/abraxas/bin/runitor -slug $(hostname)flex /home/abraxas/bin/check-my-tasks.sh 
-#>>/home/abraxas/test.dat 2>>/home/abraxas/test.dat
-
-### am here NTFY auto
-curl -d "$(hostname) $(date) EXECUTE $(cat ~/.sync_check//$(ls -la  ~/.sync_check | tail -n 1  | awk '{ print $9 }'))" https://n.yyps.de/auto
-
-### am here HC.IO
-curl https://hc-ping.com/o4zFWbG--a472NL8pc39jQ/$(hostname)
-
-
-
- 
-
-
-
-#$HOME/bin/sudo.sh rclone lsd ~/tmp
-#cd /home/abraxas/myq/ONCE/ALL
-#wget https://raw.githubusercontent.com/abraxas678/myq/master/ONCE/ALL/onceALL.sh
-#chmod +x onceALL.sh
-#/bin/bash onceALL.sh
-
-#cd /home/abraxas/myq/ONCE/$(hostname)
-#wget https://raw.githubusercontent.com/abraxas678/myq/master/ONCE/$(hostname)/once.sh
-#chmod +x once.sh
-
-#/bin/bash/ $HOME/bin/processq-1m.sh
-
