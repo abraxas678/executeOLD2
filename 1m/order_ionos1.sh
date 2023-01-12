92c3ed807713b3d8aaf349162c2eab0d8a1406f1
abraxas678 <abraxas678@gmail.com>
2023-01-12 01:40:28 +0100

    1

--- 1m/order_ionos1.sh
+++ 1m/order_ionos1.sh
@@ -1,1 +1,9 @@
 #!/bin/bash
+ts=$(date +"%s")
+LAST1h=$(cat /home/abraxas/myq/service/1m/order_ionos1.time1h)
+DIFF=$((ts-LAST1h))
+echo DIFF $DIFF
+if [[ $DIFF -gt "3600" ]]; then
+  echo
+  echo $ts >/home/abraxas/myq/service/1m/order_ionos1.time1h
+fi
