af7316427d24630c6af564ad3b0f25499e985b69
abraxas678 <abraxas678@gmail.com>
2023-01-12 02:50:43 +0100

    Update order_ionos1.sh

--- 1m/order_ionos1.sh
+++ 1m/order_ionos1.sh
@@ -5,6 +5,7 @@ DIFF=$((ts-LAST1h))
 echo DIFF $DIFF
 if [[ $DIFF -gt "3600" ]]; then
   echo
+  /bin/bash $HOME/bin/torrent-move.sh
   echo $ts >/home/abraxas/myq/service/1m/order_ionos1.time1h
   curl -d "1h ionos1 executed" https://hc-ping.com/o4zFWbG--a472NL8pc39jQ/order_ionos1/log
 fi
