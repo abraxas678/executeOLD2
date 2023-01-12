6b7c1ea483ec498463be740808a9a981b68f532c
abraxas678 <abraxas678@gmail.com>
2023-01-12 11:11:15 +0100

    Update execute.sh

--- 1m/execute.sh
+++ 1m/execute.sh
@@ -61,7 +61,7 @@ LASTio2=$(cat $HOME/tmp/execute.sh.ionos2.last)
 DIFFio2=$(($ts-$LASTio2))
 if [[ $(hostname) = *"ionos2"* ]]; then
   curl -d "ionos2 execute.sh DIFFio2: $DIFFio2" https://n.yyps.de/alert
-  if [[ $DIFF -gt "3600" ]]; then
+  if [[ $DIFFio2 -gt "3600" ]]; then
       curl hier https://n.yyps.de/alert
      /home/abraxas/bin/runitor -every=0 -api-url=https://hc-ping.com -slug=lubuntu -ping-key=o4zFWbG--a472NL8pc39jQ /bin/bash $HOME/bin/move-put.sh
      echo $ts >$HOME/tmp/execute.sh.ionos2.last
