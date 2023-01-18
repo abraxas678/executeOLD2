376d233dcda34446a0b457f0ce1efd7a58eaa513
abraxas678 <abraxas678@gmail.com>
2023-01-18 03:09:47 +0100

    Update execute.sh

--- 1m/execute.sh
+++ 1m/execute.sh
@@ -97,6 +97,7 @@ if [[ $(hostname) = *"wsl22"* ]]; then
      /home/abraxas/bin/runitor -every=0 -api-url=https://hc-ping.com -slug=wsl22 -ping-key=o4zFWbG--a472NL8pc39jQ rclone copy  /mnt/c/Users/abrax/Downloads gd:a_downloads --update -P --fast-list
      /home/abraxas/bin/runitor -every=0 -api-url=https://hc-ping.com -slug=wsl22 -ping-key=o4zFWbG--a472NL8pc39jQ rclone move /mnt/c/Users/abrax/Downloads gd:a_downloads --update -P --fast-list --min-age 4d
      /usr/bin/pueue add -- /home/abraxas/bin/runitor -every=0 -api-url=https://hc-ping.com -slug=wsl22 -ping-key=o4zFWbG--a472NL8pc39jQ /usr/bin/rclone move nas:Public/Dropbox/A_Waistedold gdc:Videos/Dropbox/A_Waistedold -P --update --fast-list -L          
+     /usr/bin/pueue add -- /home/abraxas/bin/runitor -every=0 -api-url=https://hc-ping.com -slug=wsl22 -ping-key=o4zFWbG--a472NL8pc39jQ /usr/bin/rclone move nas:Public/GREAT gdc:Videos/GREAT -P --update --fast-list -L          
      echo $ts >$HOME/tmp/wsl22_1h
   fi
 fi
