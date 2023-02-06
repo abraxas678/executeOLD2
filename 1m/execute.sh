e64686dae9cffc69b124fa5c4802dfb84f8a14b6
abraxas678 <abraxas678@gmail.com>
2023-02-06 06:30:25 +0100

    Update execute.sh

--- 1m/execute.sh
+++ 1m/execute.sh
@@ -49,6 +49,7 @@ if [[ $(hostname) = *"ionos1"* ]]; then
     /usr/bin/git commit -a -m auto
     /usr/bin/git push
     /usr/bin/restic backup /home/abraxas/docker/pocketbase/data --tag pb,keep -r rclone:gd:restic2
+    /usr/bin/rclone copy /home/abraxas/docker/pocketbase/data snas:docker/pocketbase/data -P
     curl -L https://hc-ping.com/o4zFWbG--a472NL8pc39jQ/pbbackup
     curl -k --request PATCH -H 'Content-Type: application/json' -d "{\"pbbackup\": \"$ts\"}" --url https://pocket.yyps.de/api/collections/smarthome/records/h1jnz8ntuhkbfjl
  fi
