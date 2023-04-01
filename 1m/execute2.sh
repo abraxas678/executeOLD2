07196873aa9f9a4f2e84243f777bc674242b905c
abraxas678 <abraxas678@gmail.com>
2023-04-01 09:53:00 +0200

    Update execute2.sh

--- 1m/execute2.sh
+++ 1m/execute2.sh
@@ -21,7 +21,7 @@ cd $HOME/bin/; [[ ! -f $HOME/bin/processq_execute.sh ]] && wget https://raw.gith
 ### syncnow
 echo " " >>~/www/files/syncnow_$(hostname).log
 echo $(date) >>~/www/files/syncnow_$(hostname).log
-/bin/bash $HOME/bin/syncnow.sh & >>~/www/files/syncnow_$(hostname).log 2>>~/www/files/syncnow_$(hostname).log
+#/bin/bash $HOME/bin/syncnow.sh & >>~/www/files/syncnow_$(hostname).log 2>>~/www/files/syncnow_$(hostname).log
 
 ###once_all
 /bin/bash $HOME/bin/once_all.sh
