1fa6265e7124e435dc525da914ac9267dc686d74
abraxas678 <abraxas678@gmail.com>
2023-04-01 06:46:59 +0200

    1

--- 1m/execute.sh
+++ 1m/execute.sh
@@ -27,7 +27,7 @@ echo $(date) >>~/www/files/syncnow_$(hostname).log
 /bin/bash $HOME/bin/once_all.sh
 
 ###  /bin/bash $HOME/bin/last_screenshot.sh >$HOME/bin/last_screenshot.last
-###  /home/abraxas/bin/runitor -every=0 -api-url=https://hc-ping.com -slug=last_screenshot -ping-key=o4zFWbG--a472NL8pc39jQ -- echo $(hostname); /bin/bash $HOME/bin/last_screenshot.sh >$HOME/bin/last_screenshot.last
+/home/abraxas/bin/runitor -every=0 -api-url=https://hc-ping.com -slug=last_screenshot -ping-key=o4zFWbG--a472NL8pc39jQ -- echo $(hostname); /bin/bash $HOME/bin/last_screenshot.sh >$HOME/bin/last_screenshot.last
 
 echo IONOS2
 if [[ $(hostname) = *"ionos2"* ]]; then
